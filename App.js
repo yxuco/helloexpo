5a207e8289ef6663fc579fc5942be99b54eebf33
Yueming Xu <yxucolo@gmail.com>
2022-09-19 15:42:38 -0600

    initial checkin

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
 import {
-  Text,
-  Link,
-  HStack,
-  Center,
-  Heading,
-  Switch,
-  useColorMode,
   NativeBaseProvider,
   extendTheme,
   VStack,
-  Box,
+  Box
 } from "native-base";
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign, Feather, FontAwesome } from '@expo/vector-icons';
 import NativeBaseIcon from "./components/NativeBaseIcon";
+import ProductList from "./components/ProductList";
+import LoginForm from "./components/LoginForm";
+import GitHub from "./components/GitHub";
+import ToDoList from "./components/ToDoList";
+import ToDoDetail from "./components/ToDoDetail";
+import { TodosContext } from "./components/TodosContext";
+import { getTodos } from "./components/storeAPI";
+import ToggleDarkMode from "./components/ToggleDarkMode";
 import { Platform } from "react-native";
 
 // Define the config
@@ -24,62 +30,138 @@ const config = {
 // extend the theme
 export const theme = extendTheme({ config });
 
+const Tab = createBottomTabNavigator();
+
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={{ headerShown: true }}>
+          <Tab.Screen name="ToDos" component={ToDoScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (<Feather name="list" color={color} size={size} />),
+            }} />
+          <Tab.Screen name="Products" component={ProductScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (<FontAwesome name="file-picture-o" color={color} size={size} />),
+            }} />
+          <Tab.Screen name="Users" component={UserScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (<Feather name="users" color={color} size={size} />),
+            }} />
+          <Tab.Screen name="Login" component={LoginScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (<AntDesign name="login" color={color} size={size} />),
+            }} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
 
-// Color Switch Component
-function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light"}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
+const ToDoStack = createStackNavigator();
+
+function ToDoScreen() {
+  // use Redux to manage app state
+  // initial app state used by useReducer hook
+  const todosInitialState = {
+    todos: []
+  }
+
+  // actions for updating app state used by useReducer hook
+  function todosReducer(state, action) {
+    switch (action.type) {
+      case 'get':
+        // initialize state to data loaded from persistent store
+        console.log(`receive state update of ${action.payload.length} items`);
+        return { ...state, todos: action.payload }  
+      case 'delete':
+        const filteredState = state.todos.filter(item => item.id !== action.payload.id);
+        return {
+          ...state,
+          todos: filteredState
         }
-      />
-      <Text>Light</Text>
-    </HStack>
+      case 'add':
+        const addedTodos = [...state.todos, action.payload];
+        return {
+          ...state,
+          todos: addedTodos
+        }
+      case 'edit':
+        const updatedToDo = { ...action.payload };
+        const updatedToDoIndex = state.todos.findIndex(t => t.id === action.payload.id);
+        const updatedToDos = [
+          ...state.todos.slice(0, updatedToDoIndex),
+          updatedToDo,
+          ...state.todos.slice(updatedToDoIndex + 1)
+        ];
+        return { ...state, todos: updatedToDos }
+      default:
+        return todosInitialState
+    }
+  }
+
+  // useReducer hook to hold app state and dispatch action
+  const [state, dispatch] = useReducer(todosReducer, todosInitialState);
+
+  // load data from persistent store
+  const data = getTodos();
+  console.log(`initial app state of ${data.length} items`);
+
+  // dispatch initial state data from persistent store if data was changed
+  // must use effect hook to load data at component mount due to async data retrieval
+  useEffect(() => {
+    if (data.length > 0) {
+      dispatch({ type: "get", payload: data })
+    }
+  }, [data]);
+
+  return (
+    <TodosContext.Provider value={{ state, dispatch }}>
+      <ToDoStack.Navigator initialRouteName="ToDoList">
+        <ToDoStack.Screen name="ToDoList" component={ToDoList} />
+        <ToDoStack.Screen name="ToDoDetail" component={ToDoDetail} />
+      </ToDoStack.Navigator>
+    </TodosContext.Provider>
   );
+}
+
+// Example to fetch GitHub users by REST request
+function UserScreen() {
+  return (
+    <VStack
+      flex={1} _dark={{ bg: "blueGray.900" }} _light={{ bg: "blueGray.50" }}
+      px={4} pb={5} space={3} alignItems="center" justifyContent="space-between">
+      <Box h="90%">
+        <GitHub />
+      </Box>
+      <ToggleDarkMode />
+    </VStack>
+  )
+}
+
+// Example to display list of cards
+function ProductScreen() {
+  return (
+    <VStack
+      flex={1} _dark={{ bg: "blueGray.900" }} _light={{ bg: "blueGray.50" }}
+      px={4} pb={5} space={3} alignItems="center" justifyContent="space-between">
+      <Box h="90%">
+        <ProductList />
+      </Box>
+      <ToggleDarkMode />
+    </VStack>
+  )
+}
+
+// Example login form
+function LoginScreen() {
+  return (
+    <VStack
+      flex={1} _dark={{ bg: "blueGray.900" }} _light={{ bg: "blueGray.50" }}
+      px={4} pb={5} space={3} alignItems="center" justifyContent="space-between">
+      <LoginForm />
+      <ToggleDarkMode />
+    </VStack>
+  )
 }
--- Readme.md
+++ Readme.md
@@ -1,9 +1,95 @@
-# NativeBase Expo Template
+# Hello Expo
 
-The official NativeBase template for [Expo](https://docs.expo.io/)
+This is a set of examples for developing native iOS and Addroid apps by using [React Native](https://reactnative.dev/docs/getting-started) and the [NativeBase](https://docs.nativebase.io/getting-started) component library.  The examples are developed and tested by using the [Expo](https://docs.expo.io/) app framework.
 
-## Usage
+It implements the examples in the book [Beginning React Native with Hooks](https://www.amazon.com/Beginning-React-Native-Hooks-Greg/dp/981147799X) by Greg Lim.  Read the book to understand more details of the examples.  The examples demostrate the use of [NativeBase components](https://reactnative.dev/docs/components-and-apis), as well as common features of native apps including the following:
+
+* [React Hooks](https://reactjs.org/docs/hooks-reference.html#usecontext) for managing app state, and interaction with REST services and external persistence stores.
+* [React Navigation](https://reactnavigation.org/docs/tab-based-navigation) for tab or stack based screen navigations.
+* [axios](https://axios-http.com/) for invoking REST web services.
+* [json-server](https://github.com/typicode/json-server) for testing data persistence.
+* [Expo Icons](https://icons.expo.fyi/) that can be selected to display buttons.
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS) properties that can be configured for visualization.
+
+## Development Setup
+
+Install NodeJS by using [NVM](https://github.com/nvm-sh/nvm).
 
 ```sh
-expo init my-app --template @native-base/expo-template
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install --lts
 ```
+
+On Mac, install Xcode from apple store, and then install Xcode CLI tools.
+
+```sh
+xcode-select --install
+xcode-select -p
+```
+
+Install global JS utilities used by the examples.
+
+```sh
+npm install -g expo-cli
+npm install -g json-server
+```
+
+## Start New App Project
+
+If you want to create your own project in a `WORK` folder, and code from scratch, you can get started with the following steps.
+
+```sh
+cd ${WORK}
+expo init helloexpo --template @native-base/expo-template
+```
+
+Start the app in iOS simulator
+
+```sh
+cd ${WORK}/helloexpo
+expo start --ios
+```
+
+To publish and run the app on your device, i.e., iPhone, you can follow the following steps.
+
+* Sign up at [Expo](https://expo.dev/), and create a project.
+* Edit [app.json](./app.json) in the local project so the `slug` matches the project name on the Expo dev server, and `owner` matches your Expo user name.
+* Publish app to the Expo dev server: `cd $WORK/helloexpo && expo publish`
+* On the iPhone, find and install `Expo Go` from apple store.
+* Start `Expo Go`, login, and start the previously published app.
+
+## Libraries Used by the Examples
+
+Following are libraries installed during the development of the examples in this project.
+
+```sh
+# to call REST web service
+npm install axios
+# to generate UUID
+npm install uuid-random
+# swipe list view
+npm install --save react-native-swipe-list-view
+# react screen navigation
+npm install @react-navigation/native
+npm install @react-navigation/stack
+npm install react-native-gesture-handler
+npx expo install @react-native-masked-view/masked-view
+npm install @react-navigation/bottom-tabs
+```
+
+Note that the [Expo Snack](https://snack.expo.dev/?platform=ios) can be used to test code snippets.
+
+## Demonstrate Data Persistence
+
+To test the `ToDoList` example with external data persistence, we use [json-server](https://github.com/typicode/json-server) as a test database.
+
+Start json-server:
+
+```sh
+cd ${WORK}/helloexpo
+json-server todos.json
+```
+
+Uncomment the line `const storeUrl = "http://localhost:3000/todos/"` in [storeAPI.js](./components/storeAPI.js), and then start the app in iOS simulator.
+
+Any changes to the `ToDoList` will be stored in the file [todos.json](./todos.json).  Note that dragging a ToDo list item to the left in the iOS similator would similate the swipe-left gesture, which allows you to edit or delete the item.
